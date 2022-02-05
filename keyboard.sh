67f74a45095bdf5e2a6970edf24ac4c35b9ac1f1
BabyDragoN-MarMu <83784408+BabyDragoN-MarMu@users.noreply.github.com>
2022-02-05 17:52:09 +0630

    Create keyboard.sh

---
+++ keyboard.sh
@@ -0,0 +1,146 @@
+
+#Creation By MarMu
+#Facebook Page - Technology By MarMu
+#DO NOT THIEF MY STYLE
+#DON'T COPY & MODIFY THIS CODE
+####£££££££££££££££££££££££££################
+
+#-command_not_found_handle() {
+#	/data/data/com.termux/files/usr/libexec/termux/command-not-found "$1"
+#}
+
+clear
+RD='\e[1;31m'
+
+GR='\e[1;32m'
+
+YL='\e[1;33m'
+
+BL='\e[1;34m'
+
+PP='\e[1;35m'
+
+LBL='\e[1;36m'
+
+GY='\e[1;30m'
+
+WH='\e[1;37m'
+
+clear
+figlet setup | lolcat
+#pkg installation
+pkg install ruby -y
+pkg install python -y
+pkg install figlet -y
+gem install lolcat
+pkg install toilet -y
+
+cd $HOME
+
+echo
+clear
+echo " "
+echo " "
+echo -e "\e[1m\e[31m\
+     ████████╗███████╗██████╗░███╗░░░███╗██╗░░░██╗██╗░░██╗ \e[1;91m
+     ╚══██╔══╝██╔════╝██╔══██╗████╗░████║██║░░░██║╚██╗██╔╝ \e[1;97m
+     ░░░██║░░░█████╗░░██████╔╝██╔████╔██║██║░░░██║░╚███╔╝░ \e[1;97m
+     ░░░██║░░░██╔══╝░░██╔══██╗██║╚██╔╝██║██║░░░██║░██╔██╗░ \e[1;91m
+     ░░░██║░░░███████╗██║░░██║██║░╚═╝░██║╚██████╔╝██╔╝╚██╗ \eß
+     ░░░╚═╝░░░╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░╚═════╝░╚═╝░░╚═╝
+\e[36m\
+                     🇰​​​​​🇪​​​​​🇾​​​​​🇧​​​​​🇴​​​​​🇦​​​​​🇷​​​​​🇩​​​​​
+"
+echo ""
+sleep 2.0
+pip install lolcat
+gem install lolcat
+echo " "
+echo -e $GR"                   ☆☆☆ ($  M A R M U  $GR) ☆☆☆"
+
+echo " "
+echo -e $GY"           《$GR T$gYL E$RD R$GY M$LBL U$GR X$WH  ⭐ $BL K$RD E$BL Y$GR B$PP O$LBL A$YL R$BL D$GY 》" 
+echo
+echo -e ""
+echo -e $GR " Enter Your Name : "
+read name
+echo -e "figlet -f small "$name" | lolcat" 
+echo -e "   Your Termux Keyboard Has Upgraded | lolcat"    
+sleep 3
+    
+echo " "
+
+termux-setup-storage
+pip2 install lolcat
+
+pkg install figlet -y
+echo ""
+clear
+echo "                   "DO YOU KNOW MARMU ??  | lolcat
+
+pkg install toilet -y
+cd $HOME
+sleep 2
+echo -e "\e[1m\e[31m\
+000000000000000_000000000000000
+00000000000000___00000000000000
+0000000000000_____0000000000000
+000000000000_______000000000000
+00000000000_________00000000000
+0__________ MYANMAR _________00
+000_______ ..........______ 000
+00000 ______ DRAGON______ 00000
+0000000_________________0000000
+000000_________0_________000000
+00000_______0000000_______00000
+0000_____0000000000000_____0000
+000___0000000000000000000___000
+00_0000000000000000000000000_00 \e[1;37m
+╒══════════════════⭐══════════════════╕
+ \e[1;96m    FACEBOOK   : TECHNOLOGY BY M4RMU
+ \e[1;96m    AUTHOR    : M4RMU
+ \e[1;96m    GITHUB    : B4BY DR4G0N 
+ \e[1;96m   TELEGRAM   : TERMUX MY4NM4R TEAM　\e[1;37m
+╘═══════════🍁═════════════🍁══════════╛
+"
+
+echo
+echo $BL"["$PP"+"$BL"]"$LBL" NOW ! START THE TOOL.."
+read -p "You Choose Keyboard Style1 or Style2 ? (S1/S2)" marmu
+
+if [ $marmu = S1 ]
+then
+sleep 1
+cd $HOME
+mkdir $HOME/.termux/ ;echo "extra-keys = [['ESC','/','-','HOME','UP','END'],['TAB','CTRL','ALT','LEFT','DOWN','RIGHT']]" >> $HOME/.termux/termux.properties && termux-reload-settings && sleep 1      
+fi
+
+echo
+apt update && apt upgrade -y
+pkg install nano -y
+echo " "
+sleep 0.2
+clear
+sleep 0.2
+figlet -f big  '  WE ' | lolcat
+figlet -f big '  ARE   ' | lolcat
+figlet -f big ' MYANMAR ' | lolcat
+
+echo "  REBOOT YOUR TERMUX APP (OR) TAKE A NEW SESSION" | lolcat
+echo
+sleep 0.2
+echo " "
+
+xdg-open https://www.facebook.com/marmu.007/
+
+cd $HOME
+
+sleep 0.2
+
+if [ $marmu = S2 ]
+then
+sleep 1
+cd $HOME
+mkdir $HOME/.termux/ ;echo "extra-keys = [['F1','F2','F3','F4','F5','F6','F12'],['ESC','TAB','CTRL','ALT','-','DOWN','UP']]" >> $HOME/.termux/termux.properties && termux-reload-settings && sleep 1 
+
+fi
